056fbe7ca4145db8b59e024c90eeb13edac02082
Mikael Walters <mikael.walters@gmail.com>
2017-06-26 18:43:26 -0500

    initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+
+	//Global Variables
+	//=================================================
+
+	var userCharacter;
+	var enemyCharacter;
+	var availableEnemies = [];
+
+	//Characters
+	//=================================================
+	var playableCharacters = {
+
+		"maverick" = {
+			"name": "maverick",
+			"health": 130,
+			"attack": 13,
+			"counter": 12,
+			"image": 
+		},
+
+		"goose" = {
+			"name": "goose",
+			"health": 100,
+			"attack": 7,
+			"counter": 9,
+			"image": 
+		},
+
+		"iceman" = {
+			"name": "iceman",
+			"health": 150,
+			"attack": 10,
+			"counter": 14,
+			"image": 
+		},
+
+		"merlin" = {
+			"name": "merlin",
+			"health": 120,
+			"attack": 9,
+			"counter": 9,
+			"image": ,
+		}
+	};
+
+	//Functions
+	//================================================
+
+	//Determine random attack power
+	function getRandomAttackPower() {
+		var minPower = 5;
+		var maxPower = 30;
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
